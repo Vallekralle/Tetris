044e3b7a4294973cc645838a031671cb46968e2e
Valentin <kunzvalentin4@gmail.com>
2023-01-06 11:52:07 +0100

    Rotate every tetromino with the bumper buttons on the joystick

--- stetromino.py
+++ stetromino.py
@@ -15,6 +15,37 @@ class Stetromino(Tetromino):
         self.createBlockList()
         
         
+    def rotateBlocks(self, dirNum):
+        self.rotation_list = [
+            [
+                Block(self.color, self.block_list[0].x + (self.tetromino_size * (dirNum)), self.block_list[0].y - self.tetromino_size, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[1].x + self.tetromino_size * 2 if dirNum == 1 else self.block_list[1].x, self.block_list[1].y - self.tetromino_size * 2 if dirNum == -1 else self.block_list[1].y, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[2].x - self.tetromino_size, self.block_list[2].y + (self.tetromino_size * (-(dirNum))), self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[3].x, self.block_list[3].y, self.tetromino_size, self.tetromino_size)
+            ],
+            [
+                Block(self.color, self.block_list[0].x + self.tetromino_size, self.block_list[0].y + (self.tetromino_size * (dirNum)), self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[1].x + self.tetromino_size * 2 if dirNum == -1 else self.block_list[1].x, self.block_list[1].y + self.tetromino_size * 2 if dirNum == 1 else self.block_list[1].y, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[2].x + (self.tetromino_size * (dirNum)), self.block_list[2].y -self.tetromino_size, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[3].x, self.block_list[3].y, self.tetromino_size, self.tetromino_size)
+            ],
+            [
+                Block(self.color, self.block_list[0].x + (self.tetromino_size * (-(dirNum))), self.block_list[0].y + self.tetromino_size, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[1].x - self.tetromino_size * 2 if dirNum == 1 else self.block_list[1].x, self.block_list[1].y + self.tetromino_size * 2 if dirNum == -1 else self.block_list[1].y, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[2].x + self.tetromino_size, self.block_list[2].y + (self.tetromino_size * (dirNum)), self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[3].x, self.block_list[3].y, self.tetromino_size, self.tetromino_size)
+            ],
+            [
+                Block(self.color, self.block_list[0].x - self.tetromino_size, self.block_list[0].y + (self.tetromino_size * (-(dirNum))), self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[1].x - self.tetromino_size * 2 if dirNum == -1 else self.block_list[1].x, self.block_list[1].y - self.tetromino_size * 2 if dirNum == 1 else self.block_list[1].y, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[2].x + (self.tetromino_size * (-(dirNum))), self.block_list[2].y + self.tetromino_size, self.tetromino_size, self.tetromino_size),
+                Block(self.color, self.block_list[3].x, self.block_list[3].y, self.tetromino_size, self.tetromino_size)
+            ],
+        ]
+        
+        self.block_list = self.rotation_list[self.rotInd]
+        
+        
     def createBlockList(self):
         self.block_list = [
             Block(self.color, self.x + self.tetromino_size * 4 + self.offset, self.y + self.offset, self.tetromino_size, self.tetromino_size),
